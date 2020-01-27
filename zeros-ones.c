9b4fce42c351a01ea396de1d4251ba4be6e16265
emmanoel dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-01-27 14:29:48 -0800

    new c file

---
+++ zeros-ones.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print(){
+	printf("Original ASCII    Decimal  Parity   T.Error\n");
+	printf("-------- -------- -------- -------- --------\n");
+	return;
+}
+
+int main(){
+	char fileName[64];
+	char* filename = "";
+	printf("enter a word\n");
+	read(filename);
+
+	printf("%s", &filename);
+}
+
