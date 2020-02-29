c5c503c276dad9f1bb87e31b1d5403a3debb0de3
emmanoel dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-02-28 20:47:54 -0800

    finishe lab1

--- lab1/README.txt
+++ lab1/README.txt
@@ -1,1 +1,11 @@
 created new directory to hold lab1 in the comp322 repository
+
+split main into parent and child section using if statements
+
+since the process is too fast to see a time change I made the program sleep for 3 seconds
+
+moved the print statements from main into a separate print method that can be used by the parent and the child
+
+cleaned up the code a little bit and finalized things
+
+commented on everything and made last commit
---
+++ lab1/lab1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>   /* printf, stderr, fprintf */
+#include <sys/types.h> /* pid_t */
+#include <sys/times.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>  /* _exit, fork */
+#include <stdlib.h>  /* exit */
+#include <errno.h>   /* errno */
+
+void reportStatus(int pid, int retVal, struct tms timer) {
+  /*The if statement here makes it so that we can use the same method for both
+  the parent and the child without reusing code
+  */
+  if (pid == 0) {
+    printf("START: %ld\n", time(NULL));
+    printf("PPID: %d, PID: %d\n", getppid(), getpid());
+  }
+  else if (pid > 1) {
+    printf("PPID: %d, PID: %d, ", getppid(), getpid());
+    printf("CPID: %d, RETVAL: %d\n", pid, retVal);
+
+    printf("USER: %ld, SYS:  %ld\n", timer.tms_utime, timer.tms_stime);
+    printf("CUSER:%ld, CSYS: %ld\n", timer.tms_cutime, timer.tms_cstime);
+    printf("STOP: %ld\n", time(NULL));
+  }
+}
+
+int main(void)
+{
+  pid_t  pid;
+  int status;
+  struct tms timer;
+  clock_t sinceboot;
+  clock_t times(struct tms *timer);
+
+  sinceboot = times(&timer); //setting time
+  pid = fork(); //creating fork
+
+  if (pid == 0) {
+    reportStatus(pid, WEXITSTATUS(status), timer); //calling the print method
+    sleep(3);  //putting the process to sleep to change time between start and stop
+    _exit(0);  //exit child process
+  }
+
+  else if (pid > 0) {
+    waitpid(pid, &status, WUNTRACED); //waits for the child to finish before continuing
+    //printing method for child and parent
+    reportStatus(pid, WEXITSTATUS(status), timer);
+    exit(0);
+  }
+
+  else {
+    //negative number was returned from fork() so error
+    fprintf(stderr, "can't fork, error %d\n", errno);
+    exit(EXIT_FAILURE);
+  }
+  return 0;
+}
