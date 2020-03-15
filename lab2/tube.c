41883bc91795324bf2b759933b08a350878bbabc
Emmanoel Dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-03-14 17:47:58 -0700

    made major changes to tube

--- lab2/tube.c
+++ lab2/tube.c
@@ -4,48 +4,55 @@
 #include <stdlib.h>  /* exit */
 #include <errno.h>   /* errno - number of last error */
 #include <sys/wait.h> /* waitpid() */
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
     int pipefd[2];
+    int pipefd2[2];
     pipe(pipefd);
-    pid_t  pid = fork();
+    int posSecondArg;
+    int i = 0;
+    int stat_loc;
+    int stat_loc2;
+    int childFlag = 1;
 
-    if (pid == -1)
-    {
-        /* Error:
-         * When fork() returns -1, an error happened
-         * (for example, number of processes reached the limit). */
+    pid_t  pid = fork();
+    waitpid(pid, &stat_loc, WUNTRACED);
+
+    if (pid == -1) {
         fprintf(stderr, "fork error %d\n", errno);
         exit(EXIT_FAILURE);
     }
-    else if (pid == 0)
-    {
-        /* Child process:
-         When fork() returns 0, we are in the child process */
-
+    else if (pid == 0 && childFlag == 1) { //child
         execve(argv[1], argv + 1, NULL);
+        printf("this is the childFlag %d\n", childFlag);
+        _exit(0);
     }
-    else
-    {
+    else if (pid > 0) { //parent
+        childFlag = 2;
+        printf("this is the childFlag %d\n", childFlag);
         pid_t pid2 = fork();
-        if (pid2 > 0)
-        {
-             fprintf(stderr, "%s: $$ = %d\n", argv[1], pid);
-             fprintf(stderr, "%s: $$ = %d\n", argv[4], pid2);
-        }
-        else if (pid2 == 0)
+        fprintf(stderr, "%s: $$ = %d\n", argv[1], pid);
+        fprintf(stderr, "%s: $$ = %d\n", argv[4], pid2);
+        waitpid(pid2, &stat_loc2, WUNTRACED);
 
-        {}
-        {
-
+        if (pid2 == -1) {
+            fprintf(stderr, "fork error %d\n", errno);
+            exit(EXIT_FAILURE);
         }
-        /* When fork() returns a positive number, we are in the parent process
-         (the fork return value is the PID of the newly created child process) */
-         int stat_loc;
-         fprintf(stderr, "%s: $$ = %d\n", argv[1], pid);
-         waitpid(pid, &stat_loc, 0);
-         fprintf(stderr, "%s: $? = %d\n", argv[1], stat_loc);
+        else if (pid2 == 0 && childFlag == 2) { //child 2
+            execve(argv[4], argv + 1, NULL);
+            printf("this is the childFlag %d\n", childFlag);
+            _exit(0);
+        }
+        else if (pid2 > 0){
+            fprintf(stderr, "%s: $$ = %d\n", argv[1], stat_loc);
+            fprintf(stderr, "%s: $$ = %d\n", argv[4], stat_loc2);
+        }
+
+    //fprintf(stderr, "this\n");
+        exit(0);
     }
     return 0;
 }
