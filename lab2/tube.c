36c133343fd51e64a71d77c7da3f3e92dad84990
Emmanoel Dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-03-14 19:24:47 -0700

    new tube

--- lab2/tube.c
+++ lab2/tube.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 {
     int pipefd[2];
     int pipefd2[2];
+    int wait1 = -1, wait2 = -1;
     pipe(pipefd);
     int posSecondArg;
     int i = 0;
@@ -17,8 +18,21 @@ int main(int argc, char *argv[])
     int stat_loc2;
     int childFlag = 1;
 
+    while(i < argc)
+    {
+        if(strcmp(argv[i] , ",") == 0)
+        {
+            argv[i] = NULL;
+            posSecondArg = i + 1;
+            break;
+        }
+
+        i++;
+    }
+
+
     pid_t  pid = fork();
-    waitpid(pid, &stat_loc, WUNTRACED);
+    wait1 = waitpid(pid, &stat_loc, WUNTRACED);
 
     if (pid == -1) {
         fprintf(stderr, "fork error %d\n", errno);
@@ -26,29 +40,39 @@ int main(int argc, char *argv[])
     }
     else if (pid == 0 && childFlag == 1) { //child
         execve(argv[1], argv + 1, NULL);
-        printf("this is the childFlag %d\n", childFlag);
+        //printf("this is the childFlag %d\n", childFlag);
         _exit(0);
     }
     else if (pid > 0) { //parent
-        childFlag = 2;
-        printf("this is the childFlag %d\n", childFlag);
+        //childFlag = 2;
+        //printf("this is the childFlag %d\n", childFlag);
         pid_t pid2 = fork();
-        fprintf(stderr, "%s: $$ = %d\n", argv[1], pid);
-        fprintf(stderr, "%s: $$ = %d\n", argv[4], pid2);
-        waitpid(pid2, &stat_loc2, WUNTRACED);
+        if (childFlag == 1) {
+            fprintf(stderr, "%s: $$ = %d\n", argv[1], pid);
+            fprintf(stderr, "%s: $$ = %d\n", argv[4], pid2);
+        }
+        childFlag = 2;
+
+        //fprintf(stderr, "%s: $$ = %d\n", argv[1], pid);
+        //fprintf(stderr, "%s: $$ = %d\n", argv[4], pid2);
+        //wait2 = waitpid(pid2, &stat_loc2, WUNTRACED);
 
         if (pid2 == -1) {
             fprintf(stderr, "fork error %d\n", errno);
             exit(EXIT_FAILURE);
         }
         else if (pid2 == 0 && childFlag == 2) { //child 2
-            execve(argv[4], argv + 1, NULL);
-            printf("this is the childFlag %d\n", childFlag);
+            execve(argv[posSecondArg], argv + posSecondArg, NULL);
+            //printf("this is the childFlag %d\n", childFlag);
             _exit(0);
         }
         else if (pid2 > 0){
-            fprintf(stderr, "%s: $$ = %d\n", argv[1], stat_loc);
-            fprintf(stderr, "%s: $$ = %d\n", argv[4], stat_loc2);
+
+            int exitStat1 = WEXITSTATUS(stat_loc);
+            int exitStat2 = WEXITSTATUS(stat_loc2);
+            fprintf(stderr, "%s: $? = %d\n", argv[1], exitStat1);
+            fprintf(stderr, "%s: $? = %d\n", argv[posSecondArg], exitStat2);
+            //exit(0);
         }
 
     //fprintf(stderr, "this\n");
