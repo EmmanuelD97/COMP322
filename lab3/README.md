952c10dacd908d6d39a4d41ee9f1a5d309f66e3c
emmanoel dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-03-22 18:57:38 -0700

    changed hasNonSig and added whatToCatch

--- lab3/README.md
+++ lab3/README.md
@@ -1,9 +1,19 @@
 3/20/20
+
 Started on the lab finished the makefile.
 
 3/21/20
+
 created handler function to take care of multiple types of signals
 
 created a print function to print the necessary things into stderr
 
 made a function to count how many signals were being sent in
+
+3/22/20
+
+made it so the program knows which signals to look out for
+
+cleaned up hasNonSig function a little
+
+made a global array that holds all the possible signals so that we can read from it
--- lab3/catcher.c
+++ lab3/catcher.c
@@ -4,9 +4,18 @@ Emmanoel Dermkrdichyan lab3 catcher
 
 #include <stdio.h>
 #include <sys/types.h>
-#include<signal.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/errno.h>
+#include <string.h>
+
+int terminator = 0; //term signal counter
+static const char signals [38][16] = {"SIGABRT", "SIGALRM", "SIGBUS", "SIGCHLD", "SIGCLD", "SIGCONT", "SIGEMT", "SIGFPE", "SIGHUP"
+                                  "SIGILL", "SIGINFO", "SIGINT", "SIGIO", "SIGIOT", "SIGKILL", "SIGLOST", "SIGPIPE", "SIGPOLL"
+                                  "SIGPROF", "SIGPWR", "SIGQUIT", "SIGSEGV", "SIGSTKFLT", "SIGSTOP", "SIGTSTP", "SIGSYS"
+                                  "SIGTERM", "SIGTRAP", "SIGTTIN", "SIGTTOU", "SIGUNUSED", "SIGURG", "SIGUSR1", "SIGUSR2"
+                                  "SIGVTALRM", "SIGXCPU", "SIGXFSZ", "SIGWINCH"};
+static const char signalInp[33][11] = {"HUP", "INT", "QUIT", "ILL", "TRAP", "ABRT", "BUS", "FPE", "KILL", "USR1", "SEGV", "USR2", "PIPE", "ALRM", "TERM", "STKFLT", "CHLD", "CONT", "STOP", "TSTP", "TTIN", "TTOU", "URG", "XCPU", "XFSZ", "VTALRM", "PROF", "WINCH", "IO", "PWR", "SYS", "IOT", "POLL"};
 
 void printPID() {
   int pid = getpid();
@@ -17,35 +26,42 @@ void printTotalCT(int totalSigCount) {
   fprintf(stderr, "catcher: Total signals count = %d\n", totalSigCount);
 }
 
+int whatToCatch(int argc, char **argv, int numberOfSigArgs, int indexToCatch[]) {
+  int count = 0;
+  for (int i = 1; i <= numberOfSigArgs; i++) {
+    for (int j = 0; j < 33; j++) {
+      if (strcmp(signalInp[j], argv[i]) == 0) {
+        indexToCatch[count] = j;
+        count++;
+      }
+    }
+  }
+}
+
 void handler(int sig) {
-  /*if (sig == SIGINT) {
-  }*/
+  int whichSig = 0;
 
 }
 
 int hasNonSig(int argc, char **argv) {
   if (argv[argc - 1][0] == '>' || argv[argc - 1][0] == '|') {
-    return 1;
+    return argc - 1;
   }
   else {
-    return 0;
+    return argc;
   }
 }
 
 void catcher(int argc, char **argv, int termCount, int totalSigCount) {
   int nonSig = 0;
   int argCount = argc;
-  int *toSend;
   printPID();
 
 
   nonSig = hasNonSig(argc, argv);
-  if (nonSig == 1) {
-    argCount--;
-  }
+  argCount = argCount - nonSig;
 
   for (int i = 1; i < argCount; i++) {
-    //toSend = argv[i];
     signal(*argv[i], handler);
     totalSigCount++;
   }
@@ -64,6 +80,11 @@ int main(int argc, char **argv) {
   //without resetting the values of both
   int termCount = 0; //# of term signals caught
   int totalSigCount = 0; //total # of signals caught
+  int numberOfSigArgs = (hasNonSig(argc, argv) - 1);
+  int indexToCatch[numberOfSigArgs];
+
+  whatToCatch(argc, argv, numberOfSigArgs, indexToCatch);
+
   catcher(argc, argv, termCount, totalSigCount);
   return 0;
 }
