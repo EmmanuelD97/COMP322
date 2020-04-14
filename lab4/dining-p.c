216218dac0ec14210ea8708d680fa34f66291aee
emmanoel dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-04-14 00:00:24 -0700

    done for the night

--- lab4/dining-p.c
+++ lab4/dining-p.c
@@ -2,3 +2,11 @@
 Emmanoel Dermkrdichyan Lab 4 - Rice, Rice, and Rice
 4/12/20
 */
+
+int main(int argc, char **argv) {
+  /*
+  Catcher takes care of main logic, it brings all the functions together.
+  */
+  dining(argc, argv);
+  return 0;
+}
