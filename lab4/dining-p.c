3c5eaafe5da96b66fcf868295908c058ac6d61ca
Emmanoel Dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-04-24 00:25:44 -0700

    changed semaphores

--- lab4/dining-p.c
+++ lab4/dining-p.c
@@ -14,13 +14,21 @@ Emmanoel Dermkrdichyan Lab 4 - Rice, Rice, and Rice
 #include <sys/stat.h>
 #include <fcntl.h>
 
-static int termFlag = 0;
+#define SEM_FILE1  "/leftCh"
+#define SEM_FILE2  "/RightCh"
 
+sem_t * returnLeft;
+sem_t * returnRight;
+//sem_t * chopstick[atoi(argv[1]) + 1];
+
+static int termFlag = 0;
 static unsigned long next = 1;
+
+
 
 void signalHandler(int sigNumber) {
 	signal (SIGTERM, signalHandler);
-	printf("SIGTERM %d\n", sigNumber);
+	printf("SIGTERM received\n");
 	termFlag = 1;
 }
 
@@ -41,25 +49,34 @@ void mysrand(unsigned seed) {
 void eating (int pos) {
 	printf("Philosopher #%d is eating\n", pos);
 	//take up random time usleep(3) rand(3)
-	usleep(random());
+	//usleep(random());
+	usleep(300);
 }
 
 void thinking (int pos) {
 	printf("Philosopher #%d is thinking\n", pos);
-	usleep(random());
+	//usleep(random());
+	usleep(300);
 }
 
 int dining(int argc, char **argv, sem_t chopstick[], int seats, int position) {
 	int cycleCt = 0;
+	printf("its before the signal\n");
+
 	signal(SIGTERM, signalHandler);
+	printf("its before the do while loop\n");
 
 	do {
-		sem_wait(&chopstick[position]);						  //atoi converts from string to int
-		sem_wait(&chopstick[(position + 1) % seats]); //argv[2] is number of seats
+		printf("its before semwait1\n");
+		sem_wait(returnLeft);  //atoi converts from string to int
+		printf("its before semwait2\n");
+		sem_wait(returnRight); //argv[2] is number of seats
 		eating(position);
 
-		sem_post(&chopstick[position]);
-		sem_post(&chopstick[(position + 1) % seats]);
+		printf("its before sempost1\n");
+		//sem_post(&chopstick[position]);
+		printf("its before sempost2\n");
+		//sem_post(&chopstick[(position + 1) % seats]);
 		thinking(position);
 
 		cycleCt++;
@@ -69,7 +86,7 @@ int dining(int argc, char **argv, sem_t chopstick[], int seats, int position) {
 	return cycleCt;
 }
 void termReceived(int position, int totalCycles, sem_t chopstick[], char* semN) {
-	fprintf(stderr, "Philosopher #%d completed %d cycles.", position, totalCycles);
+	fprintf(stderr, "Philosopher #%d completed %d cycles.\n", position, totalCycles);
 
 	sem_close(&chopstick[position]);
 	sem_unlink(semN); //semN needs to get changed
@@ -78,21 +95,31 @@ void termReceived(int position, int totalCycles, sem_t chopstick[], char* semN) 
 }
 
 int main(int argc, char **argv) {
+	printf("PID: %d\n", getpid());
+	printf("literally first thing in main\n");
+
 	int totalCycles;
 	int seats = atoi(argv[1]);
 	int position = atoi(argv[2]);
 	sem_t chopstick[(seats + 1)];
 
+	printf("its before the semaphore open\n");
+
+	returnLeft = sem_open(SEM_FILE1, O_CREAT|O_EXCL, 0666, 1);
+	returnRight = sem_open(SEM_FILE2, O_CREAT|O_EXCL, 0666, 1);
+
+	printf("its before the argc check\n");
 
 	if(argc != 3) {
-		printf("Wrong number of arguments. (needs 2)\n");
+		printf("Incorrect number of arguments. (needs 2)\n");
 		exit(EXIT_FAILURE);
 	}
+	else if (position > seats) {
+		printf("All seats filled\n");
+	}
 
-	//int shm_fd = shm_open(argv[2], O_CREAT | O_EXCL, 0666);
 
-
-
+	printf("its before the call to dining\n");
 
 	totalCycles = dining(argc, argv, chopstick, seats, position);
 
