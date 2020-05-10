618822f189575cdc3a98a662189aa2bb66add0bb
emmanoel dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-05-10 14:56:53 -0700

    added mole.c

--- lab6/mole.c
+++ lab6/mole.c
@@ -1,8 +1,26 @@
 /*
-Emmanoel Dermkrdichyan - lab 6 - moles.c
+Emmanoel Dermkrdichyan - lab 6 - mole.c
 4/30/20
 */
 
-void main () {
-	
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+void mole(int argc, char **argv) {
+	FILE *file = fopen("lab6.log", "a");
+	char moleN[10];
+
+	if (file != 0 && argc > 1) {
+		sprintf(moleN, "Pop mole%d", atoi(argv[1]));
+		fputs(moleN, file);
+
+		fclose(file);
+	}
+}
+
+void main (int argc, char **argv) {
+	mole(argc, argv);
 }
--- lab6/spiritd.c
+++ lab6/spiritd.c
@@ -3,6 +3,7 @@ Emmanoel Dermkrdichyan - lab 6 - spiritd.c
 4/30/20
 */
 
-void main () {
+void main (int argc, char** argv) {
+	int procRet = 0;
 
 }
