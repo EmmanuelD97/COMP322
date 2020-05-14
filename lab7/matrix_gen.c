317879c70c72d98eaef9f37a877e4032e58673a7
emmanoel dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-05-14 13:26:59 -0700

    finished matrix gen

--- lab7/matrix_gen.c
+++ lab7/matrix_gen.c
@@ -1,18 +1,31 @@
+/*
+Emmanoel Dermkrdichyan - lab 7 - matrix_gen
+5/14/20
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+Since were just using > to place the output into a file all
+we have to do is print the random numbers that we are generating
+in the shape of the matrix.
+*/
 void matrixGen(int size) {
 	int randomElement = 0;
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++){
+			//generates a random integer from -100 to 100
 			randomElement = (rand() % (100 + 100 + 1)) - 100;
+			printf("%d ", randomElement);
 		}
+		printf("\n");
 	}
 }
 
 void main (int argc, char** argv) {
 	int size = 0;
-	size = atoi(argv[1]);
+	size = atoi(argv[1]); //first argument is the size of the matrix
 
 	matrixGen(size);
 }
