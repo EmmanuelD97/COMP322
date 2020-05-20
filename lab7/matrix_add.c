6670c75db1f3fc8ba4a01ec5d1012c4e7b1d89b2
emmanoel dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-05-19 19:26:58 -0700

    rewrote a couple functions

--- lab7/matrix_add.c
+++ lab7/matrix_add.c
@@ -10,25 +10,6 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-//I got load and unload from a stackoverflow page NOT MY ORIGINAL WORK
-//these are temporary until I can fix the rest of the main method then I will
-//replace these. Not sure exactly how to rewrite these tho... seems pretty straight
-//forward tbh
-void load(struct aiocb *fill, off_t off, size_t blockSize) {
-	fill->aio_fildes = fileno(stdin);
-	fill->aio_nbytes = blockSize; //blockSize
-	fill->aio_offset = off;
-	fill->aio_buf = malloc(blockSize);
-	fill->aio_reqprio = 0;
-}
-
-void unload(struct aiocb *store, off_t off, size_t blockSize) {
-	store->aio_fildes = fileno(stdout);
-	store->aio_nbytes = blockSize;
-	store->aio_offset = off;
-}
-
-
 void matrix_add (struct aiocb* calculate, int size, int scalar, int blockSize) {
 
 }
@@ -57,15 +38,26 @@ void main (int argc, char** argv) {
 	for (i = 0; i < totChar; i = i + blockSize) {
 		if (i == 0) {
 
-			load(&calculate, 0, blockSize);
+			//load
+			calculate.aio_fildes = fileno(stdin);
+			calculate.aio_nbytes = blockSize; //blockSize
+			calculate.aio_offset = 0;
+			calculate.aio_buf = malloc(blockSize);
+			calculate.aio_reqprio = 0;
+
 			aio_read(&calculate);
 
 			while(aio_error(&calculate) == EINPROGRESS);
 
 			aio_return(&calculate);
 
-			//filling next one
-			load(&fill, i + blockSize, blockSize);
+			//load
+			fill.aio_fildes = fileno(stdin);
+			fill.aio_nbytes = blockSize; //blockSize
+			fill.aio_offset = i + blockSize;
+			fill.aio_buf = malloc(blockSize);
+			fill.aio_reqprio = 0;
+
 			aio_read(&fill);
 			while(aio_error(&fill) == EINPROGRESS);
 			aio_return(&fill);
@@ -75,7 +67,11 @@ void main (int argc, char** argv) {
 
 			//copy calculated one to write one
 			memcpy(&store, &calculate, sizeof(struct aiocb));
-			unload(&store, i, blockSize); //i - blockSize
+
+			//unload
+			store.aio_fildes = fileno(stdout);
+			store.aio_nbytes = blockSize;
+			store.aio_offset = i;
 
 			aio_write(&store);
 			while(aio_error(&store) == EINPROGRESS);
@@ -85,9 +81,13 @@ void main (int argc, char** argv) {
 			memcpy(&calculate, &fill, sizeof(struct aiocb));
 		}
 		else if (i < totChar) {
+			//load
+			fill.aio_fildes = fileno(stdin);
+			fill.aio_nbytes = blockSize;
+			fill.aio_offset = i + blockSize;
+			fill.aio_buf = malloc(blockSize);
+			fill.aio_reqprio = 0;
 
-			//filling next one
-			load(&fill, i + blockSize, blockSize);
 			aio_read(&fill);
 			while(aio_error(&fill) == EINPROGRESS);
 			aio_return(&fill);
@@ -97,7 +97,11 @@ void main (int argc, char** argv) {
 			
 			//copy calculated one to write one
 			memcpy(&store, &calculate, sizeof(struct aiocb));
-			unload(&store, i, blockSize); //i - blockSize
+
+			//unload
+			store.aio_fildes = fileno(stdout);
+			store.aio_nbytes = blockSize;
+			store.aio_offset = i;
 
 			aio_write(&store);
 			while(aio_error(&store) == EINPROGRESS);
@@ -112,7 +116,11 @@ void main (int argc, char** argv) {
 	matrix_add(&calculate, size, scalar, blockSize);
 
 	memcpy(&store, &calculate, sizeof(struct aiocb));
-	unload(&store, i, blockSize);
+
+	//unload
+	store.aio_fildes = fileno(stdout);
+	store.aio_nbytes = blockSize;
+	store.aio_offset = i;
 
 	aio_write(&store);
 	while(aio_error(&store) == EINPROGRESS);
